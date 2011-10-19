48a31ee5ede737994a38b90a8f1276240f4d558d
Christopher Vagnetoft <noccylabs@gmail.com>
2011-10-19 00:56:35 +0000

    Updated suart header file with cosmetic fixes

--- shared/suart.h
+++ shared/suart.h
@@ -1,9 +1,9 @@
 /**
- * 
+ *
  * Half Duplex Software UART on the LaunchPad
- * 
+ *
  * This code provides a simple Bi-Directional Half Duplex Software UART. The
- * timing is dependant on SMCLK, which is set to 1MHz. The transmit function 
+ * timing is dependant on SMCLK, which is set to 1MHz. The transmit function
  * is based off of the example code provided by TI with the LaunchPad. This
  * code was originally created for "NJC's MSP430 LaunchPad Blog".
  *
@@ -13,7 +13,7 @@
  * Do note that this code hooks the TIMER0 A0 interrupt as well as the PORT1
  * interrupt, and requires global interrupts to be enabled. This is done by
  * the suart_init() method.
- * 
+ *
  * @author Nicholas J. Conn - http://msp430launchpad.com @08-17-10
  * @author Christopher V - http://noccylabs.info @10-19-11
  * @license GNU General Public License (GPL) V2 or later
@@ -22,18 +22,18 @@
 
 #ifndef __SUART_H__
 #define __SUART_H__
-  
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 #include <msp430.h>
 #include <legacymsp430.h>
 
-#define TXD BIT1    // TXD on P1.1
-#define RXD BIT2	// RXD on P1.2
+#define TXD BIT1 // TXD on P1.1
+#define RXD BIT2 // RXD on P1.2
 
-#define Bit_time 104		// 9600 Baud, SMCLK=1MHz (1MHz/9600)=104
-#define Bit_time_5	52		// Time for half a bit.
+#define Bit_time 104 // 9600 Baud, SMCLK=1MHz (1MHz/9600)=104
+#define Bit_time_5 52 // Time for half a bit.
 
 #define SUART_RCVBUFSIZE 32 // Size of receive buffer
 
