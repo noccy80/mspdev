c0f96a796f9075b63a6f01ff786f958ea69fab78
Christopher Vagnetoft <noccylabs@gmail.com>
2011-12-09 15:10:03 +0000

    Added stubs for libmidi

---
+++ libs/libmidi/midi.h
@@ -0,0 +1,16 @@
+#ifndef __MIDI_H__
+#define __MIDI_H__
+
+#include <stdint.h>
+
+/**
+ * @brief Initialize the MIDI subsystem using the specified timer
+ *
+ * @param uint16_t timer The timer to use
+ */
+void midi_init(uint16_t timer);
+
+
+
+
+#endif // __MIDI_H__
