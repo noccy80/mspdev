485fe2d13eda0939dda5a702afc4b4960bd2b033
Christopher Vagnetoft <noccylabs@gmail.com>
2012-01-02 03:36:15 +0000

    Updated midi buffer code

--- lib430/src/libmidi/buffer.c
+++ lib430/src/libmidi/buffer.c
@@ -5,7 +5,15 @@ volatile uint8_t midi_rxbuffer[MIDI_RXBUFSIZE] = {0};
 volatile uint8_t midi_txbufptr = 0;
 volatile uint8_t midi_rxbufptr = 0;
 
-void midi_txbyte(uint8_t byte) {
+/** 
+ * @brief Transmit a byte over MIDI.
+ *
+ * The byte will be added to the end of the transmission buffer. If an error
+ * occurs the return value will be non-zero.
+ *
+ * @return uint8_t 0 on success
+ */
+uint8_t midi_txbyte(uint8_t byte) {
 
 	// Push the bytes on the buffer and increase the pointer
 	midi_txbuffer[midi_txbufptr] = byte;
@@ -13,6 +21,14 @@ void midi_txbyte(uint8_t byte) {
 
 }
 
+/** 
+ * @brief Transmit a byte over MIDI.
+ *
+ * The byte will be added to the end of the transmission buffer. If an error
+ * occurs the return value will be non-zero.
+ *
+ * @return uint8_t The byte on success, and 0 otherwise.
+ */
 uint8_t midi_rxbyte(void) {
 
 	// Pop a byte off the buffer if there is one
