c7bc038980bd0818dbd6c793c8d346d3ed6b548a
Christopher Vagnetoft <noccylabs@gmail.com>
2012-01-17 16:01:48 +0000

    libAir now compiles, but doesn't not really offer much besides the library. More work needed.

--- lib430/src/libair/cc1101.c
+++ lib430/src/libair/cc1101.c
@@ -1,31 +1,30 @@
-#include "cc1101.h"
+#include <cc1101.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <spi.h>
 
-int cc1101_initialize( CC1101 *radio,
-	GPIOPIN pClock, GPIOPIN pDataInBi, GPIOPIN pDataOut,
-	long options
-);
-int cc1101_power_state( CC1101 *radio, uint8_t state );
-int cc1101_set_callback( CC1101 *radio, (void*)cb );
+#define CC1101_CFG_CONFIG 0
+#define CC1101_CFG_PMODE 0
 
-SPI_CONNECTION spisess;
+SPI_SESSION spisess;
 
 int cc1101_initialize( CC1101 *radio,
 	GPIOPIN pClock, GPIOPIN pDataInBi, GPIOPIN pDataOut,
 	long options
 ) {
 	// Set up the SPI connection
-	spi_setup(&spisess, pClock, pDataInBi);
-	spi_write(&spisess,CC1101_CFG_CONFIG,CC1101_DATA_INITIAL);
+	//spi_setup(&spisess, pClock, pDataInBi);
+	//spi_write(&spisess,CC1101_CFG_CONFIG,&CC1101_DATA_INITIAL);
+	return 0;
 }
 
-int cc1101_set_callback( CC1101 *radio, (void*)cb ) {
+int cc1101_set_callback( CC1101* radio, int cb) {
 	radio->cbaddr = cb;
+	return 0;
 }
 
 int cc1101_power_state( CC1101 *radio, uint8_t state ) {
 	radio->state = state;
-	spi_write(&spisess,CC1101_CFG_PMODE,state);
+	// spi_write(&spisess,CC1101_CFG_PMODE,(const char*)state);
+	return 0;
 }
--- lib430/src/libair/cc1101.h
+++ lib430/src/libair/cc1101.h
@@ -5,19 +5,15 @@
 
 typedef struct _CC1101 {
 	int portpair;
+	int state;
+	void* cbaddr;
 } CC1101;
-typedef struct _GPIOPIN {
-	uint8_t port;
-	uint8_t pin;
-} GPIOPIN;
 
 int cc1101_initialize( CC1101 *radio,
 	GPIOPIN pClock, GPIOPIN pDataInBi, GPIOPIN pDataOut,
 	long options
 );
 int cc1101_power_state( CC1101 *radio, uint8_t state );
-int cc1101_set_callback( CC1101 *radio );
-
-SPI_SESSION spisess;
+int cc1101_set_callback( CC1101 *radio, int cb );
 
 #endif
