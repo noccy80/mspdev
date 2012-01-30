a88305fd35c6e6ec5343a6eba205c2b7999fab9a
Christopher Vagnetoft <noccylabs@gmail.com>
2012-01-30 01:30:06 +0000

    libarduino builds and works properly, #define ARDUINO_AMIN in your main file before #including <arduino.h>

--- lib430/src/libarduino/arduino.c
+++ lib430/src/libarduino/arduino.c
@@ -2,20 +2,3 @@
 #include <wdt.h>
 #include <msplib.h>
 
-void setup(void);
-void loop(void);
-
-int main(void) {
-
-	// Halt the watchdog timer - According to the datasheet the watchdog timer
-	// starts automatically after powerup. It must be configured or halted at
-	// the beginning of code execution to avoid a system reset.
-	WDTCTL = WDTPW + WDTHOLD;;
-
-	setup();
-	uptime_init();
-	for(;;){ loop(); }
-
-}
-
-
--- lib430/src/libarduino/arduino.h
+++ lib430/src/libarduino/arduino.h
@@ -4,6 +4,9 @@
 #include <msp430.h>
 #include <stdint.h>
 
+void setup();
+void loop();
+
 #include <uart.h>
 
 #ifndef WORD
@@ -90,6 +93,20 @@ void uptime_init();
 unsigned long millis();
 unsigned long micros();
 
+#ifdef ARDUINO_MAIN
+int main(void) {
+
+	// Halt the watchdog timer - According to the datasheet the watchdog timer
+	// starts automatically after powerup. It must be configured or halted at
+	// the beginning of code execution to avoid a system reset.
+	WDTCTL = WDTPW + WDTHOLD;;
+
+	setup();
+	// uptime_init();
+	for(;;){ loop(); }
+
+}
+#endif // ARDUINO_MAIN
 
 #endif // __ARDUINO_H__
 
