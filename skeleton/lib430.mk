71985e5e94df1a362841cb324dda4d4a3042f153
Christopher Vagnetoft <noccy@christopher-desktop>
2012-02-14 13:48:48 +0100

    Updated the chef

--- skeleton/lib430.mk
+++ skeleton/lib430.mk
@@ -1,4 +1,4 @@
-# To use this include, define LIB430 and have it point to the location whhere
+# To use this include, define LIB430 and have it point to the location where
 # lib430 is located. Don't forget to define LIBS for it to link as well.
 
 INCLUDES  = -I$(LIB430)/$(MCU)/include
