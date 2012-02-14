51167b1afc8063cb445b6328ae4b0a5c66507b66
Christopher Vagnetoft <noccylabs@gmail.com>
2012-02-14 04:27:25 +0000

    Added missing skeleton files

---
+++ skeleton/install.mk
@@ -0,0 +1,11 @@
+LIBINCL ?= *.h
+
+install:
+ifeq ($(DEST),)
+	echo "You must define DEST before installing"
+	exit 1
+endif
+	test -d $(DEST)/include || mkdir $(DEST)/include
+	test -d $(DEST)/lib || mkdir $(DEST)/lib
+	cp -R $(LIBINCL) $(DEST)/include
+	cp $(TARGET).a $(DEST)/lib
